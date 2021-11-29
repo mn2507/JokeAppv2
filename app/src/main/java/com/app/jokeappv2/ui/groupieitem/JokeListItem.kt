ff5269a6d250d1185bca96e2b74386015df56dae
Muthu <muthu2507@gmail.com>
2021-11-29 19:34:20 +0800

    Save jokes from jokesAPI

--- app/src/main/java/com/app/jokeappv2/ui/groupieitem/JokeListItem.kt
+++ app/src/main/java/com/app/jokeappv2/ui/groupieitem/JokeListItem.kt
@@ -2,7 +2,10 @@ package com.app.jokeappv2.ui.groupieitem
 
 import android.content.Context
 import android.view.View
+import android.widget.Toast
 import com.app.jokeappv2.data.response.Jokes
+import com.app.jokeappv2.db.JokeRepository
+import com.app.jokeappv2.db.JokeSaveModel
 import com.example.jokeappv2.R
 import com.example.jokeappv2.databinding.ItemJokeListBinding
 import com.xwray.groupie.viewbinding.BindableItem
@@ -16,14 +19,27 @@ class JokeListItem(
 
     lateinit var mContext: Context
     lateinit var mBinding: ItemJokeListBinding
+    private lateinit var jokeSaveModel: JokeSaveModel
+    private lateinit var jokeRepository: JokeRepository
 
     override fun bind(viewBinding: ItemJokeListBinding, position: Int) {
         mContext = viewBinding.root.context
         mBinding = viewBinding
+        jokeRepository = JokeRepository(mContext)
 
         mBinding.apply {
             tvJokeSetup.text = name.setup
             tvJokeDelivery.text = name.delivery
+            tvSaveJoke.setOnClickListener {
+                // Save Joke to database
+                jokeSaveModel = JokeSaveModel(id = name.id!!, setup = name.setup!!, delivery = name.delivery!!)
+                jokeRepository.saveNewJoke(jokeSaveModel)
+                Toast.makeText(
+                    mContext,
+                    "Joke has been saved!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 
